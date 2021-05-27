b9e7ff7cb32eb1b68feea4df16d368ceee4c627b
ccchippp <clempke90@gmail.com>
2021-05-26 20:01:20 -0400

    added on click event

--- src/Matrix.js
+++ src/Matrix.js
@@ -19,7 +19,8 @@ export default class Matrix extends Component {
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} 
+    selectedColor={this.state.selectedColor} />)
   )
 
   genMatrix = () => (
@@ -29,7 +30,9 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector
+          setSelectedColor={this.selectedColor}
+        />
         <div id="matrix">
           {this.genMatrix()}
         </div>
